7b8fcfacf1259fe48d61255bf4581fda907b2148
moon <nowstart2691@lguplus.co.kr>
2022-11-12 12:48:31 +0900

    Initial commit

---
+++ src/main/java/com/nowstart/teambalance/TeamBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.nowstart.teambalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBalanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamBalanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nowstart/teambalance/TeamBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowstart.teambalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamBalanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
